dec20846ac67df82ce3c2a344b11d6d9ed576da8
YesIDo49 <mehdialsidcheikh@gmail.com>
2023-11-21 10:25:03 +0100

    light mode section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": {
+            100: "#FCE7F3",
+            200: "#F0D8E2",
+            300: "#B22A65",
+            400: "#AA0F52",
+            },
+        "secondary": "#EAEAFA",
+        "light": "#F4F4F5",
+        "dark": {
+          100: "#414147",
+          200: "#232225",
+          300: "#2a2228",
+        },
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
